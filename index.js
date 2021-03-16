99e8507fe51c4ba2389be34261067da893048ab0
LavaMelon <nawrocki.p07@gmail.com>
2021-03-16 21:23:08 +0100

    DWH

---
+++ index.js
@@ -0,0 +1,71 @@
+var nick;
+var tresc;
+var linkacz;
+var wazping ="";
+var skladnia ="";
+var tresc2 ="";
+var linkdobota ="";
+
+function f1()
+{
+//pobierz
+	nick = document.getElementById("Nick").value;
+	tresc = document.getElementById("Tresc").value;
+	linkacz = document.getElementById("Linkk").value;
+	tts = document.getElementById("tts").value;
+	avatarurl = document.getElementById("ava").value;
+	
+	if (tts.toLowerCase() == "tak")
+	{tts = true;}
+	else
+	{tts = false;}
+
+	enter();
+}
+//enter
+function c() {
+	document.getElementById("sendz").innerHTML="<input class=\"send\" id=\"click\" type=\"submit\" value=\"Wyślij\" onclick=\"f1()\" style=\"float:left;\"/>";
+}
+function enter()
+{	
+
+
+	document.getElementById("sendz").innerHTML="<input class=\"send\" style=\"cursor:auto;\" id=\"click\" type=\"submit\" value=\"Poczekaj, aby uniknąć spamu\" style=\"float:left;\"/>";
+	setTimeout(function(){c();}, 1000);
+	
+	if(tresc && linkacz.includes("https://discord.com/api/webhooks"))
+	{
+		if(tresc.length<=500 && nick.length<=32){
+	const whurl = linkacz
+	const msg = {}
+	msg.tts = tts
+	msg.content = tresc
+	msg.username = nick
+	msg.avatar_url = avatarurl
+
+	
+	fetch(whurl + "?wait=true", 
+	{"method":"POST", 
+	"headers": {"content-type": "application/json"},
+	"body": JSON.stringify(msg)})
+	.then(a=>a.json()).then(console.log)
+	
+	tresc = "";
+	tresc2 = "";
+	
+	document.getElementById("cmd").innerHTML = "Jeżeli link do bota jest poprawny, to wszystko działa!";
+	document.getElementById("cmd2").innerHTML = "<img src=\"v.gif\">";
+	
+		} else {
+		/*<img src=\"v.gif\">*/
+		/*<img src=\"x.gif\">*/
+		document.getElementById("cmd").innerHTML = "Wykryto błąd, link albo treść nie są poprawne!";
+		document.getElementById("cmd2").innerHTML = "<img src=\"x.gif\">";
+	}
+	} else {
+		/*<img src=\"v.gif\">*/
+		/*<img src=\"x.gif\">*/
+		document.getElementById("cmd").innerHTML = "Wykryto błąd, link albo treść nie są poprawne!";
+		document.getElementById("cmd2").innerHTML = "<img src=\"x.gif\">";
+	}
+}
